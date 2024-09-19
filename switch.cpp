b458b9c3f3f94737524f0a9c6276bccb6cb8e974
MikisTh <114635629+MikisTh@users.noreply.github.com>
2024-09-19 09:56:19 -0300

    Create switch.cpp
    Add

---
+++ switch.cpp
@@ -0,0 +1,19 @@
+char var = 'b';
+
+switch (var) {
+  case 'a':
+    cout << "Caso 1." << endl;
+    break;
+  case 'b':
+    cout << "Caso 2." << endl;
+    break;
+  case 'c':
+    cout << "Caso 3." << endl;
+    break;
+  case 'd':
+    cout << "Caso 4." << endl;
+    break;
+  default:
+    cout << "Caso 5." << endl;
+    break;
+}
